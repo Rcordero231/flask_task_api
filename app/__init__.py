375f3b2d0ccaa1fe4cec333fea548a0820876720
Ricardo <rcordero231@gmail.com>
2024-01-16 05:53:23 -0800

    midway through second hw asssignment

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy
+migrate = Migrate()
 app = Flask(__name__)
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from fake_data.tasks import tasks_list
-
+from flask import request
+from datetime import datetime
 
 
 
@@ -21,3 +22,28 @@ def get_task_id(task_id):
         if task['id'] == task_id:
             return task
     return {'Error': f"Task with ID {task_id} does NOT exist"}, 404
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    data = request.is_json
+    required_fields = ['title', 'description']
+    missing_fields = []
+    for fields in required_fields:
+        if fields not in data:
+            missing_fields.append(fields)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+
+    title = data.get('title')
+    description = data.get('description')
+    new_tasks = {
+        "id": len(tasks_list) +1,
+        'title': title,
+        "description": description,
+        'completed': False,
+        "createdAt": datetime.utcnow()
+    }
+    tasks_list.append(new_tasks)
+    return new_tasks, 201
